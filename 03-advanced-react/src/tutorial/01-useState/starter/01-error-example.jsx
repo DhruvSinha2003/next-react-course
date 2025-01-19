6563d7a0a0b7199b5631d8004fc9417df332263f
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-19 23:15:35 +0530

    useState

--- 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h2>useState error example</h2>
+      <h3>Counter</h3>
+      <h4>{count}</h4>
+      <button onClick={handleClick}>Increment counter</button>
+    </>
+  );
 };
 
 export default ErrorExample;
--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,21 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+import { useState } from "react";
+
+const ErrorExample = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h2>useState error example</h2>
+      <h3>Counter</h3>
+      <h4>{count}</h4>
+      <button className="btn" onClick={handleClick}>
+        Increment counter
+      </button>
+    </>
+  );
 };
 
-export default UseStateBasics;
+export default ErrorExample;

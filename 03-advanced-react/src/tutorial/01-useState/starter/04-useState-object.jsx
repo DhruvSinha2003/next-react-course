6e0999b597121a5af6b841caf56a5f7ad29b0047
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-20 11:24:44 +0530

    Object

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("Dhruv");
+  const [age, setAge] = useState(21);
+  const [hobby, setHobby] = useState("Jackin offffff");
+
+  const displayPerson = () => {
+    setName("John");
+    setAge(25);
+    setHobby("Not nobbing it");
+  };
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <h3>{age}</h3>
+      <h4>Enjoys: {hobby}</h4>
+      <button className="btn" onClick={displayPerson}>
+        Change person
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;

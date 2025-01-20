acdee5fd5241da79d8be091e66185b08176b8231
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-20 12:23:35 +0530

    object

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  const [name, setName] = useState("Dhruv");
-  const [age, setAge] = useState(21);
-  const [hobby, setHobby] = useState("Jackin offffff");
+  // const [name, setName] = useState("Dhruv");
+  // const [age, setAge] = useState(21);
+  // const [hobby, setHobby] = useState("Jackin offffff");
+
+  const [obj, setObj] = useState({
+    name: "Dhruv",
+    age: 21,
+    hobby: "Jacking offfff",
+  });
 
   const displayPerson = () => {
-    setName("John");
-    setAge(25);
-    setHobby("Not nobbing it");
+    setObj({
+      name: "John",
+      age: 28,
+      hobby: "Not nobbing it",
+    });
   };
 
   return (
     <>
-      <h2>{name}</h2>
-      <h3>{age}</h3>
-      <h4>Enjoys: {hobby}</h4>
+      <h2>{obj.name}</h2>
+      <h3>{obj.age}</h3>
+      <h4>Enjoys: {obj.hobby}</h4>
       <button className="btn" onClick={displayPerson}>
         Change person
       </button>

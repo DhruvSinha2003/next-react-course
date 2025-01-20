c0f4c1a20c6a71f77b84672e9c252034bc6e4c45
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-21 00:18:34 +0530

    useeff 1

--- 03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  useEffect(() => {
+    console.log("Only during initial render");
+  }, []);
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>

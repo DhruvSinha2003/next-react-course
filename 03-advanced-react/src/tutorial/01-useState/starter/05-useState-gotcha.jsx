4ceafa8a7031047f812a1db07e9f7c3a1ef8108a
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-20 12:29:14 +0530

    gotcha

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    console.log("Before setCount ", count);
+    setCount(count + 1);
+    console.log("After setCount ", count);
+  };
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={handleClick} className="btn">
+        Increment
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;

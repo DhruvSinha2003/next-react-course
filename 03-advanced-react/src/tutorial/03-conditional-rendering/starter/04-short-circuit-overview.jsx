29a60ce1e0592a87b5834cac2c007b29a85f2dfc
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-21 20:00:02 +0530

    -

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [s1, setS1] = useState("");
+  const [s2, setS2] = useState("Dhruv");
+
+  return (
+    <div>
+      <h4>1: {s1 || "hello"}</h4>
+      <h4>2: {s2 || "hello"}</h4>
+      <h4>3: {s1 && "hello"}</h4>
+      <h4>4: {s2 && "hello"}</h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;

a896396d32d4c71468403e98cb543f8611418cc5
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-21 21:38:46 +0530

    -

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [bool, setBool] = useState(false);
+  const handleToggle = () => {
+    setBool(!bool);
+  };
+
+  return (
+    <div>
+      <h1 style={{ margin: "30px" }}>Toggle Challenge</h1>
+      {bool ? (
+        <div style={{ backgroundColor: "green" }}>
+          <h3>The value of the bool is set to positive</h3>
+        </div>
+      ) : (
+        <div style={{ backgroundColor: "red" }}>
+          <h3>The value of the bool is set to negative</h3>
+        </div>
+      )}
+      <button className="btn" onClick={handleToggle}>
+        Toggle
+      </button>
+    </div>
+  );
 };
 
 export default ToggleChallenge;

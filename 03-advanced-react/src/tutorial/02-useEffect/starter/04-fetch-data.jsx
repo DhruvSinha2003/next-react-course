eab3e59cd6f257f6ff3955f9be1a957fc5082f81
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-21 09:05:47 +0530

    fetch data

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
+import { useEffect, useState } from "react";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+
+        const data = await res.json();
+        console.log(data);
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>Github Users</h2>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <h1>{user.login}</h1>;
+            <img
+              src={user.avatar_url}
+              alt={user.login}
+              style={{ maxWidth: "200px", maxHeight: "200px" }}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 export default FetchData;

0449ccd3fa92d320b648ff839fb80667500724ce
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-22 10:53:35 +0530

    tours project

--- 04-fundamental-projects/02-tours/starter/src/App.jsx
+++ 04-fundamental-projects/02-tours/starter/src/App.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 const url = "https://www.course-api.com/react-tours-project";
 
 const App = () => {
+  const [tours, setTours] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error("Error Fetching");
+        }
+        const data = await res.json();
+        setTours(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <div>Tours Starter</div>;
 };
 export default App;

957032fecdbf7575d4cdfb703c8a5ac2eb00db0f
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-23 12:52:39 +0530

    menu

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -3,10 +3,22 @@ import data from "./data";
 import Menu from "./Menu";
 const App = () => {
   const [menuItems, setMenuItems] = useState(data);
+  const categories = data.map((item) => item.category);
+  const categorySet = new Set(categories);
+  const categoryList = ["all", ...categorySet];
 
   return (
     <div>
       <h1>Menu</h1>
+      <div>
+        {categoryList.map((category) => {
+          return (
+            <button className="btn" style={{ margin: "10px" }}>
+              {category}
+            </button>
+          );
+        })}
+      </div>
       <Menu menu={menuItems} />
     </div>
   );

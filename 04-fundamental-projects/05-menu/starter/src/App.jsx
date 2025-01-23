490f24d988df20d2666803a55391eec502c88732
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-23 13:40:10 +0530

    menu

--- 04-fundamental-projects/05-menu/starter/src/App.jsx
+++ 04-fundamental-projects/05-menu/starter/src/App.jsx
@@ -3,9 +3,19 @@ import data from "./data";
 import Menu from "./Menu";
 const App = () => {
   const [menuItems, setMenuItems] = useState(data);
+  const [categoryFilter, setCategoryFilter] = useState("all");
   const categories = data.map((item) => item.category);
   const categorySet = new Set(categories);
   const categoryList = ["all", ...categorySet];
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(data);
+      return;
+    }
+    const newItems = data.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <div>
@@ -13,13 +23,19 @@ const App = () => {
       <div>
         {categoryList.map((category) => {
           return (
-            <button className="btn" style={{ margin: "10px" }}>
+            <button
+              className="btn"
+              style={{ margin: "10px" }}
+              onClick={() => {
+                filterItems(category);
+              }}
+            >
               {category}
             </button>
           );
         })}
       </div>
-      <Menu menu={menuItems} />
+      <Menu menu={menuItems} category={categoryFilter} />
     </div>
   );
 };
--- 04-fundamental-projects/05-menu/starter/src/Menu.jsx
+++ 04-fundamental-projects/05-menu/starter/src/Menu.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import MenuItem from "./MenuItem";
 
-const Menu = ({ menu }) => {
+const Menu = ({ menu, category }) => {
   return (
     <div>
+      <h3>Menu for category: {category}</h3>
       {menu.map((menuItem) => {
         return <MenuItem key={menuItem.id} menuItem={menuItem} />;
       })}

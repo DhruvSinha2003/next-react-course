d7f3fcbb6573d809711d99f8a5fe0b1d43308b52
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-23 23:06:46 +0530

    slider

--- 04-fundamental-projects/07-slider/starter/src/Carousel.jsx
+++ 04-fundamental-projects/07-slider/starter/src/Carousel.jsx
@@ -4,16 +4,33 @@ import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { list, longList, shortList } from "./data";
 
 const Carousel = () => {
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         return (
-          <article className="slide" key={person.id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+            }}
+            key={person.id}
+          >
             <img className="person-img" src={person.image} alt={person.name} />
             <h5 className="name">{person.name}</h5>
             <p className="title">{person.title}</p>

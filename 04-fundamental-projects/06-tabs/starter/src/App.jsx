945c651cc6ec177aae8913c3ef424e2b41d2142b
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-23 18:35:24 +0530

    tabs

--- 04-fundamental-projects/06-tabs/starter/src/App.jsx
+++ 04-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,36 @@
-const url = 'https://www.course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+const url = "https://www.course-api.com/react-tabs-project";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Error while fetching the data");
+      }
+      const res = await response.json();
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading please wait</h1>
+      </div>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;

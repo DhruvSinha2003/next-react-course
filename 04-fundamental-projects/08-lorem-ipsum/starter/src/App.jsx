63e5949cecb271f55322c41d6004a97519cef90d
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-25 10:37:37 +0530

    li

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  return (
+    <div>
+      <h3>Form</h3>
+      <form className="form">
+        <label htmlFor="paragraph" className="form-label">
+          Number of paragraphs
+        </label>
+        <input type="number" id={count} className="form-input" />
+        <button className="btn" style={{ margin: "10px" }}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 export default App;

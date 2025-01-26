d26253b57ee998f124cb791262beba57e38ac811
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-26 23:40:48 +0530

    before delete form

--- 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
+++ 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import Form from "./Form";
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+  const [item, setItem] = useState("");
+
+  const Form = () => {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+    return (
+      <form type="submit">
+        <label htmlFor="item">Item</label>
+        <input type="text" />
+        <button className="btn" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    );
+  };
 };
 
 export default App;

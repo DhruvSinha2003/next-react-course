73e37f663cb8f17356525bd0f86957371bd30b79
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-01-28 11:03:02 +0530

    groc bud finished

--- 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
+++ 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   const [item, setItem] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (e.target.value === "") {
+      toast("Enter an item!");
+    }
   };
   return (
     <div>
+      <h1>Grocery Bud</h1>
       <form type="submit">
         <label htmlFor="item">Item</label>
         <input type="text" />
@@ -14,6 +21,7 @@ const App = () => {
           Submit
         </button>
       </form>
+      <ToastContainer />
     </div>
   );
 };

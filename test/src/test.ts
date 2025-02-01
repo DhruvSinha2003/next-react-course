fcaa7bb0853153481f90aaeefb63b893aa7ade70
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-02 00:14:53 +0530

    ts

---
+++ test/src/test.ts
@@ -0,0 +1,12 @@
+let firstName : string = 'Dhruv'
+let lastName : string = 'Sinha';
+
+const fullName = firstName.concat(' ').concat(lastName)
+console.log(fullName)
+
+let total : 10|20|30 = 10
+total = 30;
+
+let notSure : any = false;
+notSure = 'hmm';
+

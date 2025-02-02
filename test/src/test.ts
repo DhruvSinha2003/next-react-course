f34b86c9c9fa0a52e76cf757b63d229291a185cf
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-02 23:36:39 +0530

    func

--- test/src/test.ts
+++ test/src/test.ts
@@ -26,28 +26,38 @@ console.log(foundBook);
 // let anyArray = [1,'asd',false]
 // let car : {brand:string; year: number} = { brand: 'honda', year:2024}
 
-
 let book = {title:'book', price:20}
 let pen = {title:'pen', price:10}
 let notebook = {title:'notebook'} 
 
 let items: {readonly title:string, price:number}[] = [book,pen] //cant add notebook
 let itemss: {title:string, price?:number}[] = [book,pen,notebook] 
-
 // items[0].title = 'new'
-
 
 let bike : {brand: string, year: number} = {brand: "honda", year: 2025}
 let laptop : {brand: string, year: number} = {brand: "acer", year: 2024}
-
 let headphone: {brand:string} = {brand:"sony"}
-
 let products : {brand:string, year?:number}[]= [bike,laptop,headphone]
-
 products.push({brand:"nike"})
 
 function sayHello(name: string) {
     console.log(`Hello, ${name}`)
 }
-
 sayHello("Dhruv")
+
+function applyDiscount(price:number) : number {
+    return price*0.9
+}
+
+console.log(applyDiscount(300));
+
+const names : string []= ['Dhruv','ABC',"XYZ"];
+
+function checkName (name : string) : boolean {
+    if( names.includes(name) ){
+        return true
+    }
+    return false;
+}
+
+console.log("Check Name: "+ checkName("XYZ"))

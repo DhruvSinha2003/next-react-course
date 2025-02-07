e5b8f9706a460d1ef6d8fdc4c061afaa8d59281f
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2025-02-08 01:35:08 +0530

    before local storage

--- test/src/tasks.ts
+++ test/src/tasks.ts
@@ -1,1 +1,37 @@
-console.log("tasks")
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+const formInput = document.querySelector<HTMLInputElement>(".form-input");
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+type Task = {
+    description: string;
+    isCompleted: boolean;
+}
+
+const tasks: Task[] = []
+
+taskForm?.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const taskDescription = formInput?.value;
+    if(taskDescription){
+        const task : Task = {
+            description : taskDescription,
+            isCompleted: false
+        }
+        addTask(task)
+        renderTask(task)
+        formInput.value="";
+        return;
+    }else{
+        alert('Please enter a task description')
+    }
+})
+
+function addTask(task:Task): void {
+    tasks.push(task)
+}
+
+function renderTask(task:Task): void {
+    const taskElement = document.createElement('li')
+    taskElement.textContent = task.description;
+    taskListElement?.appendChild(taskElement);
+}
